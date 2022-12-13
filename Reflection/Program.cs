dfe3a8c62c7ae98ee370aad8c0c0a00cc162ba75
PHOENIX-5\jim <jim@phoenix-5.com>
2022-12-13 12:20:31 +0800

    呼叫方法

--- Reflection/Program.cs
+++ Reflection/Program.cs
@@ -1,16 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using SqlServerDB;
 using System.Reflection;
 
-Console.WriteLine("Hello, World!");
-
 // method 1 在執行目錄下(not working)
-Assembly assembly1 = Assembly.Load("SqlServerDB");
+//Assembly assembly1 = Assembly.Load("SqlServerDB");
 
 // method 2 專案加入引用
 
 // method 3 完整路徑
-Assembly assembly2 = Assembly.LoadFile(@"C:\Users\jim\source\repos\Reflection\SqlServerDB\bin\Debug\net6.0\SqlServerDB.dll");
+//Assembly assembly2 = Assembly.LoadFile(@"C:\Users\jim\source\repos\Reflection\SqlServerDB\bin\Debug\net6.0\SqlServerDB.dll");
 
 // method 4 建議
 //Assembly assembly3 = Assembly.LoadFrom(@"C:\Users\jim\source\repos\Reflection\SqlServerDB\bin\Debug\net6.0\SqlServerDB.dll");
 Assembly assembly3 = Assembly.LoadFrom(@"SqlServerDB.dll");
+
+//-----------------------------------------------------------------------------------------------------------------------------------//
+
+// 取得類型
+Type type = assembly3.GetType("SqlServerDB.ReflectionTest");
+
+// 靜態實例化
+ReflectionTest reflectionTest = new ReflectionTest();
+// 動態實例化
+// method 1 無參數方法呼叫
+//object objTest1 = Activator.CreateInstance(type);
+
+// method 2 有參數方法呼叫
+object objTest1 = Activator.CreateInstance(type, new object[] {"123"});
+
+Console.Read();
+
+
---
+++ SqlServerDB/ReflectionTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlServerDB
+{
+    /// <summary>
+    /// 反射測試類
+    /// </summary>
+    public class ReflectionTest
+    {
+        public ReflectionTest()
+        {
+            Console.WriteLine("無參數構造方法");
+        }
+
+        public ReflectionTest(string name)
+        {
+            Console.WriteLine($"有參數構造方法 參數值是: {name}");
+        }
+    }
+}
