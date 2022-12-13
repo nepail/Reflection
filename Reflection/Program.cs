4356f3f5a3638c774628460d6a63e93c6c602a8d
PHOENIX-5\jim <jim@phoenix-5.com>
2022-12-13 12:11:38 +0800

    反射載入DLL

--- Reflection/Program.cs
+++ Reflection/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection;
+
 Console.WriteLine("Hello, World!");
+
+// method 1 在執行目錄下(not working)
+Assembly assembly1 = Assembly.Load("SqlServerDB");
+
+// method 2 專案加入引用
+
+// method 3 完整路徑
+Assembly assembly2 = Assembly.LoadFile(@"C:\Users\jim\source\repos\Reflection\SqlServerDB\bin\Debug\net6.0\SqlServerDB.dll");
+
+// method 4 建議
+//Assembly assembly3 = Assembly.LoadFrom(@"C:\Users\jim\source\repos\Reflection\SqlServerDB\bin\Debug\net6.0\SqlServerDB.dll");
+Assembly assembly3 = Assembly.LoadFrom(@"SqlServerDB.dll");
